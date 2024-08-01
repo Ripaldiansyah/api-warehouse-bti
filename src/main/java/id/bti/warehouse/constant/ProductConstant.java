8b50a4e2d9b00dade553b02f6ad5a4dbcfb460f5
Ripaldiansyah <ripaldiansyah13@gmail.com>
2024-08-01 13:37:02 +0700

    ADD : ProductConstant

---
+++ src/main/java/id/bti/warehouse/constant/ProductConstant.java
@@ -0,0 +1,8 @@
+package id.bti.warehouse.constant;
+
+public class ProductConstant {
+
+    public final static String TIPE_PAKAIAN = "PAKAIAN";
+    public final static String TIPE_ELEKTRONIK = "ELEKTRONIK";
+
+}
