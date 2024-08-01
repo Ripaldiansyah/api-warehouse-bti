5a813e7c0b69dce3dcf7e963bd70cbf221211ea3
Ripaldiansyah <ripaldiansyah13@gmail.com>
2024-08-01 12:25:35 +0700

    UPDATE : API for get all user

--- src/main/java/id/bti/warehouse/controller/UserController.java
+++ src/main/java/id/bti/warehouse/controller/UserController.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import id.bti.warehouse.entity.User;
-import id.bti.warehouse.repository.UserRepository;
 import id.bti.warehouse.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping
     public ResponseEntity<List<User>> getAllUser() {
         List<User> allUser = userService.getAllUser();
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(allUser);
